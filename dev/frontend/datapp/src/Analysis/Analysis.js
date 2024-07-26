c795837acb2c4628db552781da532aba1bc219e5
riku1021 <iriku63@gmail.com>
2024-07-27 03:30:54 +0900

    円グラフの表示（途中）

--- dev/frontend/datapp/src/Analysis/Analysis.js
+++ dev/frontend/datapp/src/Analysis/Analysis.js
@@ -1,39 +1,61 @@
-import React, { useState } from 'react'
-import Custom from './Custom'
-import './Analysis.css'
-import Plt from './Plt'
-import Chat from './Chat'
+import React, { useState } from 'react';
+import Custom from './Custom';
+import './Analysis.css';
+import Plt from './Plt';
+import Chat from './Chat';
 
 function Analysis() {
   const [scatter, setScatter] = useState(true);
   const [histogram, setHistogram] = useState(false);
   const [boxhidediagram, setBoxhidediagram] = useState(false);
+  const [pieChart, setPieChart] = useState(false);
   const [image, setImage] = useState('');
 
   const selectionScatter = () => {
     setScatter(true);
     setHistogram(false);
     setBoxhidediagram(false);
-  }
+    setPieChart(false);
+  };
 
   const selectionHistogram = () => {
     setScatter(false);
     setHistogram(true);
     setBoxhidediagram(false);
-  }
-  
+    setPieChart(false);
+  };
+
   const selectionBoxhidediagram = () => {
     setScatter(false);
     setHistogram(false);
     setBoxhidediagram(true);
-  }
+    setPieChart(false);
+  };
+
+  const selectionPieChart = () => {
+    setScatter(false);
+    setHistogram(false);
+    setBoxhidediagram(false);
+    setPieChart(true);
+  };
+
   return (
     <div className='analysis-wrapper'>
-      <Custom selectionScatter={selectionScatter} selectionHistogram={selectionHistogram} selectionBoxhidediagram={selectionBoxhidediagram} scatter={scatter} histogram={histogram} boxhidediagram={boxhidediagram} setImage={setImage} />
+      <Custom
+        selectionScatter={selectionScatter}
+        selectionHistogram={selectionHistogram}
+        selectionBoxhidediagram={selectionBoxhidediagram}
+        selectionPieChart={selectionPieChart}
+        scatter={scatter}
+        histogram={histogram}
+        boxhidediagram={boxhidediagram}
+        pieChart={pieChart}
+        setImage={setImage}
+      />
       <Plt image={image} />
       <Chat />
     </div>
-  )
+  );
 }
 
-export default Analysis
+export default Analysis;
